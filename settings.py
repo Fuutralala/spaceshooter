b6666af6596b72f700d2653e497dfdd669dddf8c
Fuu Tralala <nighthawk0@outlook.de>
2019-07-20 02:18:02 +0200

    nothing real
    just changed my local folder an somehow gitkraken wants to make a new commit out of this

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class to store all settings for the game."""
+
+    def __init__(self):
+        """Initializes the game's settings."""
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+
+        self.ship_speed_factor = 1.5
