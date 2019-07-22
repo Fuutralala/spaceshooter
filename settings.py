e34831831b2bba171761e13232ad51465bc2f7a2
Fuu Tralala <nighthawk0@outlook.de>
2019-07-22 04:30:12 +0200

    Space invaders clone
    managed to make the game sort of playable

--- settings.py
+++ settings.py
@@ -1,10 +1,24 @@
 class Settings():
-    """A class to store all settings for the game."""
 
     def __init__(self):
-        """Initializes the game's settings."""
+        # Screen settings.
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
+        # Ship settings.
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # Bullet settings.
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Alien settings.
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
